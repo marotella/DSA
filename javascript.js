878cb075ce50c32ce3915363c21ecdbc680283ef
marotella <michelle.a.rotella@gmail.com>
2023-04-24 22:07:59 -0400

    timeout

--- javascript.js
+++ javascript.js
@@ -13,3 +13,16 @@ var createCounter = function(n) {
         
     };
 };
+
+//Given a positive integer millis, write an asyncronous function that sleeps for millis milliseconds. It can resolve any value.
+
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise ((resolve) =>{
+    let t = Date.now()
+    setTimeout(() => {
+        resolve(console.log(Date.now() - t))}, millis)
+    })
+}
