3a2b3e2f223f177e2b87b73bad6e046fab97c851
marotella <michelle.a.rotella@gmail.com>
2023-07-23 20:56:55 -0400

    Middle value of linked list

--- linkedlist.py
+++ linkedlist.py
@@ -46,3 +46,36 @@ class LinkedList:
           current_node = None
         else:
           current_node = next_node
+          
+          
+### Middle value of a linked list:
+# Complete the find_middle() function and return the middle node of linked_list. You can assume that the linked list has no cycles.
+
+# Return the right-weighted middle for even-length linked lists. For example, in a linked list of 4 elements it should return the element at the third position.
+
+# Use generate_test_linked_list(length) to generate linked lists with values from 1 -> 2 -> .. -> length to test out your function. For instance, generate_test_linked_list(4) results in 1 -> 2 -> 3 -> 4.
+
+from LinkedList import LinkedList
+
+# Complete this function:
+def find_middle(linked_list):
+  fast = linked_list.head_node
+  slow = linked_list.head_node
+  while fast:
+    fast = fast.get_next_node()
+    if fast:
+      fast = fast.get_next_node()
+      slow = slow.get_next_node()
+  return slow
+
+def generate_test_linked_list(length):
+  linked_list = LinkedList()
+  for i in range(length, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list(7)
+print(test_list.stringify_list())
+middle_node = find_middle(test_list)
+print(middle_node.value)
