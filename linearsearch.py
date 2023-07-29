51441ee5aff37afe0b8ec27d3153a7343c5ad9ff
marotella <michelle.a.rotella@gmail.com>
2023-07-29 10:51:20 -0400

    linear search practice

--- linearsearch.py
+++ linearsearch.py
@@ -9,3 +9,21 @@ def linear_search(search_list, target_value):
 # uncomment the lines below to test...
 values = [54, 22, 46, 99]
 print(linear_search(values, 22))
+
+
+
+def linear_search(search_list, target_value):
+  for idx in range(len(search_list)):
+    if search_list[idx] == target_value:
+      return idx
+  raise ValueError("{0} not in list".format(target_value))
+
+
+
+try:
+  # Call the function below...
+  result = linear_search(number_list, target_number)
+  print(result)
+  linear_search(number_list, 100)
+except ValueError as error_message:
+  print("{0}".format(error_message))
