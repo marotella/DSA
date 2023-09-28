95735fd131b5a369ad51eb0975809e0b869ff527
marotella <michelle.a.rotella@gmail.com>
2023-09-28 18:51:55 -0400

    min max

---
+++ Week 2/minmaxarr.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    # Write your code here
+    # sort array
+    arr.sort()
+    #create an unfairness variable
+    unfairness = float('inf')
+    #iterate through the array to build the subarray
+    for i in range(len(arr)- (k-1)):
+        subarr = arr[i:k+i]
+    #find the min and max
+        difference = max(subarr)-min(subarr)
+    #subtract
+    #compare to unfairness
+        if difference < unfairness:
+            unfairness = difference
+    #store
+    #return
+    return unfairness
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
