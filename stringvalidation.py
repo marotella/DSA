cd936f8278ae89da637b6b5c545c7049f8bdbc8f
marotella <michelle.a.rotella@gmail.com>
2023-10-30 20:56:15 -0400

    string validator

---
+++ stringvalidation.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    s = input()
+        
+    print(s.isalnum())
+    print(s.isalnum())
+    print(s.isalnum())
+    for letter in s:
+        if letter.islower():
+            print(letter.islower())
+    for letter in s:
+        if letter.isupper():
+            print(letter.isupper())
